6f693eb2d3dff3c3831ead1bb809a23498f7d792
Carolina Guaman <acguamana@unal.edu.co>
2024-04-06 11:12:02 -0500

    Test 7 Docker

--- src/7b_docker_client/app.py
+++ src/7b_docker_client/app.py
@@ -82,7 +82,8 @@ def index():
 
 if __name__ == "__main__":
     logging.info("Starting the application")
-    app.run(debug=True, port=5002)
-    logging.info("Starting the application")
+    app.run(debug=True, port=5000, host="0.0.0.0")
+    logging.info("Finishing the application")
+
 
     
